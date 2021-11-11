07f1cca4ec13315efeeb73f25b6c30feccbeee72
Alejandro <alejandrolealastorga@gmail.com>
2021-11-11 13:34:18 +0100

    Docu de arrays

---
+++ Arrays.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	Scanner sc = new Scanner(System.in);
+	
+	int [] num;
+	
+	num = new int [5];
+	
+	num[0] = 2; 
+	num[2] = 67;
+	num[1] = 666;
+	num[3] = 534;
+	num[4] = 1312;
+	
+	
+	for (int i = 0; i < num.length; i++) {
+		System.out.println(num [i]);
+		
+	}
+	
+	
+	}
+
+}
